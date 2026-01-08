9fb5a68b57d7eeafcc5deef84f3226a547e56601
Jan Markvart <redstgp@gmail.com>
2026-01-08 18:00:49 +0100

    for operation refactored

--- src/interpreter/ControlFlow.js
+++ src/interpreter/ControlFlow.js
@@ -1,15 +1,16 @@
 //control flow operations (if, for, repeat)
 
 import * as VAR_api from "./VariableOperations.js";
+import * as BoxerParser from "./BoxParser.js";
 
 //--------------------------------------------------------------------------------
     // Contro Flow Primitive operations
 //--------------------------------------------------------------------------------
 
 var control_primitives = {
-    "if" : {function: CF_if, needs_variables: false},
+    "if" : {function: CF_if, needs_variables: true},
     "for" : {function: CF_for, needs_variables: true},
-    "repeat" : {function: CF_repeat, needs_variables: false}
+    "repeat" : {function: CF_repeat, needs_variables: true}
 }
 
 export function importPrimitives()
@@ -21,19 +22,37 @@ export function CF_if(...operands)
 {
     let operations = [];
 
-    return operations;
+    let res = {
+        "return_type": "operations",
+        "return_value": operations
+    }
+    return res;
 }
 
 export function CF_for(variables, ...operands)
 {
     let operations = [];
 
-    return operations;
+    let res = {
+        "return_type": "operations",
+        "return_value": operations
+    }
+    return res;
 }
 
-export function CF_repeat(times, code)
+export function CF_repeat(variables, times, box)
 {
     let operations = [];
-
-    return operations;
+    times = Number(VAR_api.processOperand(variables, times));
+    if(isNaN(times)) { alert("Repeat: first operand has to be a number!"); return; }
+    for(let i = 0; i < times; i++)
+    {
+        let new_operations = BoxerParser.BoxerParser(box);
+        operations = operations.concat(new_operations);
+    }
+    let res = {
+        "return_type": "operations",
+        "return_value": operations
+    }
+    return res;
 }
